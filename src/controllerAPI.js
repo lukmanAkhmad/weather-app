815a438df430d25bd4c69b741dcce4f20c2a8732
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-07-28 07:37:42 +0700

    feat: default unit

--- src/controllerAPI.js
+++ src/controllerAPI.js
@@ -8,7 +8,7 @@ async function getData(cityName = "jakarta") {
   createLoadingComponent();
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=FFF5MD2W854NT8CK43BZBB5TT`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=FFF5MD2W854NT8CK43BZBB5TT`,
       { mode: "cors" },
     );
     const jsonResponse = await response.json();
